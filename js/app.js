ffd40998dc6157b2c87443e003196daeced6d0b9
tacksoo <tacksoo.im@gmail.com>
2014-01-19 23:27:24 -0500

    added bootstrap

--- js/app.js
+++ js/app.js
@@ -1,14 +1,17 @@
-App = Ember.Application.create();
+App = Ember.Application.create({
+  LOG_TRANSITIONS : true
+});
 
 App.Router.map(function() {
   // put your routes here
-  this.route("about", { path: "/about"});
+  this.route("about", {
+    path : "/about"
+  });
 });
 
 App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['Java', 'JavaScript', 'Ruby'];
+  model : function() {
+    return ['Java', 'JavaScript', 'Ruby', 'Scala'];
   }
 });
-
 
