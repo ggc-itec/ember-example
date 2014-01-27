be584cd8ccfa99536acb792e6cd4225171e95a99
tacksoo <tacksoo.im@gmail.com>
2014-01-26 22:17:19 -0500

    added some routes

--- js/app.js
+++ js/app.js
@@ -3,15 +3,61 @@ App = Ember.Application.create({
 });
 
 App.Router.map(function() {
-  // put your routes here
-  this.route("about", {
-    path : "/about"
+  this.resource('about');
+  this.resource('currency');
+  this.resource('stocks', function () {
+    this.resource('stock', { path: ':stock_id' });    
   });
+  this.resource('activity');
+  
 });
 
-App.IndexRoute = Ember.Route.extend({
-  model : function() {
-    return ['Java', 'JavaScript', 'Ruby', 'Scala'];
+App.CurrencyRoute = Ember.Route.extend({
+  model: function() {
+    return currencies;
   }
 });
 
+App.StocksRoute = Ember.Route.extend({
+  model: function() {
+    return stocks;    
+  }
+});
+
+
+App.StockRoute = Ember.Route.extend({
+  model: function(params) {
+    return stocks.findBy('id',params.stock_id);
+  }
+});
+
+App.ActivityRoute = Ember.Route.extend({
+  model: function() {
+    return $.getJSON('http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?&format=json').then(function(data) {
+      return data;
+    });     
+  }
+});
+
+var currencies = [
+{
+  id:'1',
+  from: "KRW",
+},
+{
+  id:'2',
+  from: "YEN" 
+}];
+
+
+var stocks = [
+{
+  id:'1',
+  quote:'YHOO',
+  description: 'Yahoo! an internet company' 
+},
+{
+  id:'2',
+  quote:'TWTR', 
+  description: 'Twit at Twitter'
+}];
